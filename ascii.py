db149df916c833b94edb1fd01df92bff0da08ac9
unknown <patrik.moukola@tuni.fi>
2025-01-20 15:03:27 +0200

    updates to matrix pouring

--- ascii.py
+++ ascii.py
@@ -16,25 +16,11 @@ c = ' .:-=+*#%@'
 #max_width = 100
 #max_height = 100
 
-
-# Read the image and convert it to grayscale
-image = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
-#image = cv2.imread('test2.jpg', cv2.IMREAD_GRAYSCALE)
-print("original shape:", image.shape)
-
-
-# Calculate the new height to maintain aspect ratio
-original_height, original_width = image.shape
-image_aspect_ratio = original_height / original_width  # Image aspect ratio (height/width)
-image_scale_factor = 0.1
-print("Image aspect ratio:", image_aspect_ratio)
-character_aspect_ratio = 2  # Approximate height-to-width ratio of characters
-
 #c = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#$%&()*+,-./:;<=>?@[\]^_`{|}~                                  '
 
 
 # Create a function to map grayscale values to characters
-def map_to_char(value):
+def map_to_char(value, scale_factor):
     index = int(value / scale_factor)  # Scale the value to the character set
     return c[index]
 
@@ -74,21 +60,21 @@ def print_table(chartable):
         for char in row:
             print(char, end='')
         print()
-
-while True:
-    #print(keyboard.read_key())
-    
-    if keyboard.is_pressed('+'):
-        print("increasing image scale factor")
-        image_scale_factor += 0.05
-    if keyboard.is_pressed('-'):
-        print("decreasing image scale factor")
-        image_scale_factor -= 0.05
-    if image_scale_factor < 0.05:
-        image_scale_factor = 0.05
         
-    print(image_scale_factor)
-    
+
+
+def image_to_ascii(image_scale_factor = 0.1):
+    # Read the image and convert it to grayscale
+    image = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
+    #image = cv2.imread('test2.jpg', cv2.IMREAD_GRAYSCALE)
+    print("original shape:", image.shape)
+
+
+    # Calculate the new height to maintain aspect ratio
+    original_height, original_width = image.shape
+    image_aspect_ratio = original_height / original_width  # Image aspect ratio (height/width)
+    print("Image aspect ratio:", image_aspect_ratio)
+    character_aspect_ratio = 2  # Approximate height-to-width ratio of characters
     adjusted_height = int((image_scale_factor*original_height)/character_aspect_ratio)
     adjusted_width = int(image_scale_factor*original_width)
     
@@ -101,17 +87,43 @@ while True:
     
     # Replace each pixel with the corresponding character
     height, width = resized_image.shape
-    original_char_image = np.array([[map_to_char(resized_image[y, x]) for x in range(width)] for y in range(height)])
-    print("ascii shape", original_char_image.shape)
+    original_char_image = np.array([[map_to_char(resized_image[y, x], scale_factor) for x in range(width)] for y in range(height)])
+    return original_char_image
 
-    # Combine the characters into lines to visualize as text
-    ascii_art = "\n".join("".join(row) for row in original_char_image)
-    char_image = np.copy(original_char_image)
+
+image_scale_factor = 0.1
+original_char_image = image_to_ascii(image_scale_factor)
+char_image = np.copy(original_char_image)
+max_chance = 0.1
+weights = [max_chance / (i + 1) for i in range(original_char_image.shape[0])]
+#weights.reverse()
+
+print(original_char_image.shape)
+print(weights)
+time.sleep(2)
+
+
+
+while True:
+    #print(keyboard.read_key())
+    """
+    if keyboard.is_pressed('+'):
+        print("increasing image scale factor")
+        image_scale_factor += 0.05
+    if keyboard.is_pressed('-'):
+        print("decreasing image scale factor")
+        image_scale_factor -= 0.05
+    if image_scale_factor < 0.05:
+        image_scale_factor = 0.05
+    """ 
     
+        
+    
+    
+    """
     #print(char_image)
     
     #--------------------
-    """
     # IMAGE SCROLLING DOWN REPEATEDLY
     while True:
         for i, row in reversed(list(enumerate(char_image))):
@@ -122,32 +134,46 @@ while True:
                     char_image[i][j] = char_image[i-1][j]
         os.system('cls' if os.name == 'nt' else 'clear')
         print_table(char_image)
-        time.sleep(0.05)"""
+        time.sleep(0.05)
+    """
     
-    pour_symbol = " "
+    #pour_symbol = random.choice("|*xoO1")
+    pour_symbol = random.choice("|")
     
-    while True:
-        for i, row in reversed(list(enumerate(char_image))):
-            for j, char in enumerate(row):
-                #print(i,j)
-                if random.randint(0,1000) == 0:
-                    char_image[i][j] = pour_symbol
+    for i, row in reversed(list(enumerate(char_image))):
+        for j, char in enumerate(row):
+            #print(i,j)
+            #the chance of droplets is increased the closer we are to the top
+            #print(i, j)
+            #print(random.random(), weights[i])
+            #i : 37...0
+            #[0.9 ... 0]
+            if weights[i]/100 > random.random():
+                char_image[i][j] = pour_symbol
+                pour_length = random.randint(1,3)
+                if i > pour_length:
+                    for cell in range(pour_length):
+                        char_image[cell][j] = pour_symbol
+            
+            if weights[i]+0.3 > random.random():
                 if i != 0:
-                    #print(i)
-                    if random.randint(0,10) == 0:
-                        if char_image[i-1][j]  == pour_symbol:
-                            char_image[i][j] = pour_symbol
-                
-                if random.randint(0,20) == 0:
-                    char_image[i][j] = original_char_image[i][j]
+                    if char_image[i-1][j]  == pour_symbol:
+                        char_image[i][j] = pour_symbol
+            
+            if i  > random.randint(0,100):
+                char_image[i][j] = original_char_image[i][j]
+        
+    os.system('cls' if os.name == 'nt' else 'clear')
+        
+        
+    poured_ascii_art = "\n".join("".join(row) for row in char_image)
+    print(poured_ascii_art)
     
-        #time.sleep(10)
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print_table(char_image)
-        ascii_art = "\n".join("".join(row) for row in original_char_image)
-        print(ascii_art)
-        #30fps
-        time.sleep(0.02)
+    original_ascii_art = "\n".join("".join(row) for row in original_char_image)
+    print(original_ascii_art)
+    #30fps
+    time.sleep(0.02)
+    
     """
     while True:
         for i, row in reversed(list(enumerate(char_image))):
@@ -159,12 +185,5 @@ while True:
     """
                         
     
-        
-    
-    #populate_table(ascii_art)
-    
-    # Print the ASCII art
-    print(ascii_art)
-    time.sleep(1)
     
 
